39f9f456c3d61e754de95d849950e8348fd24b18
ololoshka2871 <SweetTreasure@2ch.hk>
2024-11-24 13:17:43 +0300

    some verbose configuration

--- src/config.rs
+++ src/config.rs
@@ -2,8 +2,11 @@ pub const XTAL_FREQ: u32 = 8_000_000;
 
 //-----------------------------------------------------------------------------
 
-// usb pull up
-//pub const USB_PULLUP_ACTVE_LEVEL: bool = false;
+//usb pull up
+pub const USB_PULLUP_ACTVE_LEVEL: Option<bool> = None;
+
+pub const USB_VID: u16 = 0x16c0;
+pub const USB_PID: u16 = 0x314f;
 
 //-----------------------------------------------------------------------------
 
